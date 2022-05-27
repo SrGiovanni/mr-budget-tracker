3b76e80e5b16821fc145984b0eacd95ec36cccb9
SrGiovanni <samuelefulghi@gmail.com>
2022-05-26 21:17:02 -0400

    remove errant console log

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,6 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
       caches.open(CACHE_NAME).then(function (cache) {
         console.log('installing cache : ' + CACHE_NAME)
-        console.log(FILES_TO_CACHE)
         return cache.addAll(FILES_TO_CACHE)
       })
     );
